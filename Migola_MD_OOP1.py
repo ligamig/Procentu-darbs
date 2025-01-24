709c4815ff1ff227f0a216c46b85fadb663f2e86
Liga <ligamig2@gmail.com>
2025-01-24 12:49:26 +0200

    3

--- Migola_MD_OOP1.py
+++ Migola_MD_OOP1.py
@@ -1,4 +1,32 @@
 #1. uzdevums
 class turnirs:
-    def __init__(self, nosaukums, cilvSkaits, grupuSkaits, sportaVeids, sponsori):
+    def __init__(self, nosaukums, cilvSkaits, grupuSkaits, sportaVeids,):
+        self.nosaukums =  nosaukums
+        self.cilvSkaits = cilvSkaits
+        self.grupuSkaits = grupuSkaits
+        self.sportaVeids = sportaVeids
+        self.sponsori = []
+
+    def SponsoruSaraksts(self, sponsori):
+        self.sponsori.append(sponsori)
         
+    def izvade(self):
+        sponsors = "\n".join(self.sponsori)
+        return (
+            f"Šis ir {self.sportaVeids} turnīrs \"{self.nosaukums}\", kurā piedalās {self.cilvSkaits} cilvēki, {self.grupuSkaits} grupas. \n"
+            f"Sponsori: \n{sponsors} "
+        )
+
+t1 = turnirs("Last Dab 2025", 18, 5, "Tortnite")
+t1.SponsoruSaraksts("Adidaš")
+t1.SponsoruSaraksts("Mike")
+t1.SponsoruSaraksts("DolčeNKabana")
+print(t1.izvade())
+
+print("-----")
+
+t2 = turnirs("Spray & pray", 20, 4, "Valoban")
+t2.SponsoruSaraksts("Red Wool")
+t2.SponsoruSaraksts("Bego")
+t2.SponsoruSaraksts("Ronster")
+print(t2.izvade())
